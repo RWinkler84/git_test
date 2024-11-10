e058e6be34e03fc4022df4749f8af97d366f76b5
RWinkler84 <winkler.ralf84@hotmail.de>
2024-11-10 01:36:00 +0100

    -started building a start menu, mainly styling it

--- rechnungstool/index.php
+++ rechnungstool/index.php
@@ -3,6 +3,8 @@ include_once 'initialData.php';
 
 include_once 'html_templates/header.html';
 
+include_once 'html_templates/topMenu.html';
+
 include_once 'html_templates/invoiceForm.html';
 
 include_once 'html_templates/footer.html';
